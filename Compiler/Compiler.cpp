364d94f15eaeee899a97fc1ba62f2f1d797c260c
MeulenG <Mollern2000@outlook.dk>
2022-08-09 20:09:30 +0200

    Working on the Lexer

--- Compiler/Compiler.cpp
+++ Compiler/Compiler.cpp
@@ -5,8 +5,36 @@
 
 using namespace std;
 
-int main()
+void ReadFile() {
+#ifdef READFILE
+    string filename = argv[1];
+    ifstream input;
+
+    input.open(filename);
+    // Check input parameters
+    if (argc == 1) {
+        printf("Haxor: invalid number of arguments\n");
+        return -1;
+    }
+    else if (!input.is_open()) {
+        cout << "Unable to open file";
+        return -1;
+    }
+    else {
+        while (input)
+        {
+            string line;
+            getline(input, line);
+            cout << line << '\n';
+        }
+    }
+    input.close();
+#endif
+}
+int main(int argc, char* argv[])
 {
+    ReadFile();
+
 	cout << "Hello CMake." << endl;
 	return 0;
 }
--- Compiler/Compiler.h
+++ Compiler/Compiler.h
@@ -4,5 +4,8 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "../external/llvm-project/llvm/include/llvm/IR/IRBuilder.h"
 
 // TODO: Reference additional headers your program requires here.
