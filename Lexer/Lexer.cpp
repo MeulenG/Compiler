d07083f995a2ef07fa68479f5896c0c6d58311e8
MeulenG <Mollern2000@outlook.dk>
2022-09-15 22:37:04 +0200

    Removing all the if statements and replacing them with map

--- Lexer/Lexer.cpp
+++ Lexer/Lexer.cpp
@@ -3,21 +3,181 @@
 
 using namespace std;
 
-string check_String_Token;
-double check_Number_Token;
-
 void Error() {
 	// just an error code for now
 	cout << "Error" << endl;
 }
 
-int split_Program_To_Tokens(bool filename) {
+bool check_File(bool filename_Is_Valid) {
+	// Check for valid file
+	if (check_String_Token != "begin" && filename_Is_Valid)
+	{
+		Error();
+	}
+	else { return true; }
+}
+
+
+
+
+
+// Pretty Trivial
+bool is_Space(char c) noexcept {
+	switch (c)
+	{
+	case ' ':
+	case '\t':
+	case '\r':
+	case '\n':
+		return true;
+	default:
+		return false;
+	}
+}
+
+
+// Pretty Trivial
+bool is_Digit(char c) {
+	switch (c)
+	{
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+		return true;
+	default:
+		return false;
+	}
+}
+
+// Pretty brute-force probably gonna rewrite this shit later
+bool is_Identifier_Char(char c) {
+	switch (c)
+	{
+	case 'a':
+	case 'b':
+	case 'c':
+	case 'd':
+	case 'e':
+	case 'f':
+	case 'g':
+	case 'h':
+	case 'i':
+	case 'j':
+	case 'k':
+	case 'l':
+	case 'm':
+	case 'n':
+	case 'o':
+	case 'p':
+	case 'q':
+	case 'r':
+	case 's':
+	case 't':
+	case 'u':
+	case 'v':
+	case 'w':
+	case 'x':
+	case 'y':
+	case 'z':
+	case 'A':
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'E':
+	case 'F':
+	case 'G':
+	case 'H':
+	case 'I':
+	case 'J':
+	case 'K':
+	case 'L':
+	case 'M':
+	case 'N':
+	case 'O':
+	case 'P':
+	case 'Q':
+	case 'R':
+	case 'S':
+	case 'T':
+	case 'U':
+	case 'V':
+	case 'W':
+	case 'X':
+	case 'Y':
+	case 'Z':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	case '_':
+		return true;
+	default:
+		return false;
+	}
+}
+
+// Map math symbols
+map<char, int> token_Map = {
+	{'+', token_PLUS},
+	{'-', token_MINUS},
+	{'*', token_TIMES},
+	{'/', token_DIVIDE},
+	{'(', token_LPARENTHE},
+	{')', token_RPARENTHE},
+	{'[', token_LBRACKET},
+	{']', token_RBRACKET},
+	{'{', token_LCURLYBRAC},
+	{'}', token_RCURLYBRAC},
+	{'=', token_EQUAL},
+	{',', token_COMMA},
+	{':', token_COLON},
+	{';', token_SEMICOLON},
+	{'<', token_LTH},
+	{'>', token_RTH},
+	{'!', token_NOT},
+	{'&', token_AND},
+	{'|', token_OR},
+};
+
+// Map keywords
+map<string, int> keyword_Map = {
+	{"if", token_IF},
+	{"else", token_ELSE},
+	{"while", token_WHILE},
+	{"return", token_RETURN},
+	{"int", token_INT},
+	{"float", token_FLOAT},
+	{"true", token_TRUE},
+	{"false", token_FALSE},
+	{"struct", token_STRUCT},
+	{"start", token_SOF},
+	{"end", token_EOF},
+};
+
+
+
+
+int split_Program_To_Tokens() {
 	int last_Char = ' '; // last character read
 
 	// Skip any whitespace
 	while (isspace(last_Char)) {
 		last_Char = getchar();
 	}
+	
 
 	if (isalpha(last_Char)) {
 		// identifier or keyword
@@ -26,12 +186,6 @@ int split_Program_To_Tokens(bool filename) {
 		while (isalnum(last_Char = getchar())) {
 			check_String_Token += last_Char;
 		}
-		// Validate file
-		if (check_String_Token == "start", filename)
-		{
-			return token_SOF, filename;
-		}
-		else { cout << "Can't identify the start of your Haxor Program :(" << endl; }
 
 		// Check if it is a math symbol
 		if (check_String_Token == "+") {
@@ -150,7 +304,7 @@ int split_Program_To_Tokens(bool filename) {
 			while (last_Char != EOF && last_Char != '\n' && last_Char != '\r'); // read until newline
 
 			if (last_Char != EOF)
-				return split_Program_To_Tokens(filename); // read next token
+				return split_Program_To_Tokens(); // read next token
 		}
 
 		// Check for end of file. Don't eat the EOF.
@@ -160,4 +314,5 @@ int split_Program_To_Tokens(bool filename) {
 		// Otherwise, just return the character as its ascii value
 		return last_Char;
 	}
+	
 }
--- Lexer/Lexer.h
+++ Lexer/Lexer.h
@@ -2,56 +2,101 @@
 #define LEXER_H
 
 #include <iostream>
-#include <fstream>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
 #include <string>
+#include <utility>
 #include <vector>
+
+using namespace std;
+
+// Global Variables for our Lexer
+string check_String_Token;
+double check_Number_Token; // Check Decimal values like "1.0"
+int check_Number_Token_Int; // Check Integers like "1"
+
+
+class Lexer
+{
+public:
+	virtual ~Lexer();
+	bool filename_Is_Valid;
+	
+	
+public:
+	bool is_Space(char c) { }
+	bool is_Digit(char c) { }
+	bool is_Identifier_Char(char c) { }
+	int check_File(bool filename_Is_Valid) {  }
+	int split_Program_To_Tokens() {  }
+};
+
 
 // Tokens for our program
 enum ProgramTokens
 {
 	// Identifier
 	token_IDENTIFIER = 0, // identifiers
-	// Digits
-	token_DIGIT = 1, // digit
+	// Numbers
+	token_DIGIT = 1,
+	token_NUM_LITERAL = 2, // Literal
+	token_INTEGER = 3, // Integer
 	// Math Symbols
-	token_LPARENTHE = 2, // (
-	token_RPARENTHE = 3, // )
-	token_LBRACKET = 4, // [
-	token_RBRACKET = 5, // ]
-	token_LCURLYBRAC = 6, // {
-	token_RCURLYBRAC = 7, // }
-	token_EQUAL = 8, // =
-	token_COMMA = 9, // ,
-	token_COLON = 10,// :
-	token_SEMICOLON = 11,// ;
-	token_PLUS = 12,
-	token_MINUS = 13,
-	token_TIMES = 14,
-	token_DIVIDE = 15,
-	token_ARROW = 16, // =>
-	token_DEQ = 17, // ==
-	token_LTH = 18, // <
-	token_RTH = 19, // >
-	token_AND = 20, // &&
-	token_OR = 21, // ||
-	token_NOT = 22, // !
-	// keywords
-	token_IF = 23,
-	token_THEN = 24,
-	token_ELSE = 25,
-	token_INT = 26,
-	token_TRUE = 27,
-	token_FALSE = 28,
-	token_STRUCT = 29,
-	token_ENUM = 30,
-	token_FUNCTION = 31,
-	token_DEFINE = 32,
-	token_USING = 33,
+	token_LPARENTHE = 4, // (
+	token_RPARENTHE = 5, // )
+	token_LBRACKET = 6, // [
+	token_RBRACKET = 7, // ]
+	token_LCURLYBRAC = 8, // {
+	token_RCURLYBRAC = 9, // }
+	token_EQUAL = 10, // =
+	token_COMMA = 11, // ,
+	token_COLON = 12,// :
+	token_SEMICOLON = 13,// ;
+	token_PLUS = 14,
+	token_MINUS = 15,
+	token_TIMES = 16,
+	token_DIVIDE = 17,
+	token_ARROW = 18, // =>
+	token_DEQ = 19, // ==
+	token_LTH = 20, // <
+	token_RTH = 21, // >
+	token_AND = 22, // &&
+	token_OR = 23, // ||
+	token_NOT = 24, // !
+	// Keywords
+	// Statements
+	token_IF = 25,
+	token_THEN = 26,
+	token_ELSE = 27,
+	token_WHILE = 28,
+	// Int, float
+	token_INT = 29,
+	token_FLOAT = 30,
+	token_DOUBLE = 31,
+	// Bools
+	token_TRUE = 32,
+	token_FALSE = 33,
+	// Classic struct which we all love
+	token_STRUCT = 34,
+	// The classic enum
+	token_ENUM = 35,
+	// Tokens for making a function
+	token_FUNCTION = 36,
+	token_BEGIN = 37,
+	token_END = 38,
+	token_RETURN = 39,
+	// Token for defining a variable
+	token_DEFINE = 40,
+	// Token for importing a file or library
+	token_USING = 41,
 	// string
-	token_QUOTE = 34, // ""
+	token_QUOTE = 42, // ""
 	// Validate file
-	token_SOF = 35, // start of file
-	token_EOF = 36, // end of file
+	token_SOF = 43, // start of file
+	token_EOF = 44, // end of file
 };
 
 #endif
